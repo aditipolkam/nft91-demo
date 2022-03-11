874ce1c6bdaa6feb94564c293fb9ff5ebef1127f
Aditi Polkam <aditipolkam@gmail.com>
2022-03-11 12:36:16 +0530

    [ADD] upload file to ipfs and get token uri

--- scripts/create_token.py
+++ scripts/create_token.py
@@ -5,9 +5,7 @@ from scripts.ipfs_upload import upload_to_ipfs
 
 def main():
     tokenName = "demo token"  # input("Enter the token name: ")
-    fileLocation = Path(
-        "C:\\Users\\aditi\\Documents\\wday.jpg"
-    )  # Path(input("Enter the file location: "))
+    fileLocation = "./img/wday.jpg"  # (input("Enter the file location: "))
     listingPrice = 0.025  # int(input("Enter the listing price: "))
     tokenURI = upload_to_ipfs(fileLocation)
     print(tokenURI)
---
+++ scripts/ipfs_upload.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import requests
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as file:
+        file_bytes = file.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url + endpoint, files={"file": file_bytes})
+        ipfs_hash = response.json()["Hash"]
+        filename = filepath.split("/")[-1:][0]
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        return image_uri
